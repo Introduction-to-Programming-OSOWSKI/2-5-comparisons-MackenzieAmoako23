d8dabea9d93792cc2802408f95f8805438694c83
MackenzieAmoako23 <831590@mystma.org>
2021-01-07 16:18:22 +0000

    2-5 Comparisons -1

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+def lessThan(x, y):
+    if x < y:
+        return True
+    else:
+        return False
+
+def equalTo(x, y):
+    if x == y:
+        return True
+    else: 
+        return False
+def greaterOrEqual(x, y):
+    if x >= y:
+        return True
+    else:
+        return False
+
+def lessOrEqual(x, y):
+    if x <= y:
+        return True
+    else:
+        return False
